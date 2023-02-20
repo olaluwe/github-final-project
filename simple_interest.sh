a098c5c79c0f5c79161f97a2a805e8053b93bece
olaluwe <21983320+olaluwe@users.noreply.github.com>
2023-02-20 18:22:46 -0500

    Create simple_interest.sh
    Create simple_interest.sh, a script to compute simple interest

---
+++ simple_interest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+# This script takes to arguments
+# namely: arg1 - principal
+#         arf2 - interest
+# MAX_ARS is the maximum number arguments
+MAX_ARGs=4
+
+# re is the expression for either integer or decimal
+re='^[0-9]+([.][0-9]+)?$'
+re2='^[0-9]+$'
+
+if [ "$#" -ne $MAX_ARGs ] || [[ "$1" =~ $re ]] || [[ "$2" =~ $re ]] || [[ "$3" =~ $re ]]
+  echo "Usage: $0 [principat] [interest_rate]" >&2;
+  exit 1
+else
+  interest=$(bc<<<"$1 * $2 * $3");
+  echo "The simple interest is: \$$interest.";
+  exit 0
